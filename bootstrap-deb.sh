b7e3a9a71d91a19fa2b0adad7ac1413a19379e33
Josh Glendenning <josh@q-desktop-nix.(none)>
2014-08-19 23:33:16 -0500

    +Added bootstrap for debian, updated Doxfile with linuxbrew task

---
+++ bootstrap-deb.sh
@@ -0,0 +1,14 @@
+#! /bin/sh
+#
+# bootstrap-deb.sh
+# Copyright (C) 2014 josh <josh@q-desktop-nix>
+#
+# Distributed under terms of the MIT license.
+#
+
+sudo apt-get install git
+git clone git@github.com:joshglendenning/profile.git ~/profile
+sudo apt-get install ruby
+sudo gem install shaddox
+cd ~/profile
+shaddox install
