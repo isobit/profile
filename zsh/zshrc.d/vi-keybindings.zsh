e2eb28acef20ff91b992b61d5f658fc62192a92b
Josh Glendenning <joshglendenning@gmail.com>
2018-07-20 22:36:31 -0500

    Add right prompt indicator for vi mode in zsh, remove buggy cursor indicator

--- zsh/zshrc.d/vi-keybindings.zsh
+++ zsh/zshrc.d/vi-keybindings.zsh
@@ -15,32 +15,14 @@ if [[ "$ZSH_VI_MODE" = true ]]; then
 	autoload -Uz edit-command-line
 	zle -N edit-command-line
 	bindkey -M vicmd v edit-command-line
-	# Use vertical cursor when in insert mode, block when in normal mode
 
-	if [[ "$TERM" =~ "screen.*" ]]; then
-		local cursor_esc_block="\eP\e[1 q\e\\"
-		local cursor_esc_line="\eP\e[5 q\e\\"
-	elif [[ "$TERM_PROGRAM" =~ "iTerm.*" ]]; then
-		local cursor_esc_block="\E]50;CursorShape=0\C-G"
-		local cursor_esc_line="\E]50;CursorShape=1\C-G"
-	else
-		local cursor_esc_block="\E[1 q"
-		local cursor_esc_line="\E[5 q"
-	fi
-
-	function zle-keymap-select zle-line-init {
-		case "$KEYMAP" in
-			vicmd)      print -n -- "$cursor_esc_block";;
-			viins|main) print -n -- "$cursor_esc_line";;
-		esac
+	# Right prompt indicator for vi mode
+	precmd() { RPROMPT="" }
+	function zle-line-init zle-keymap-select {
+		VI_MODE_PROMPT='%{$fg_bold[yellow]%}[NORMAL]%{$reset_color%}'
+		RPS1="${${KEYMAP/vicmd/$VI_MODE_PROMPT}/(main|viins)/}"
 		zle reset-prompt
-		zle -R
 	}
 	zle -N zle-line-init
 	zle -N zle-keymap-select
-
-	function zle-line-finish {
-		print -n -- "$cursor_esc_block"
-	}
-	zle -N zle-line-finish
 fi
