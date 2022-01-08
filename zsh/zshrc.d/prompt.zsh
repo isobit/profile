86435bc42bd1e848f0990adc31e6dc43589a7e2e
Josh Glendenning <git@isobit.io>
2022-01-08 02:30:59 -0600

    Add nix-shell prompt segment for ZSH

--- zsh/zshrc.d/prompt.zsh
+++ zsh/zshrc.d/prompt.zsh
@@ -12,12 +12,16 @@ precmd_functions+=(vcs_info)
 setopt prompt_subst
 local PROMPT_HOST='%{$fg_bold[grey]%}[%{$reset_color%}%{$fg[magenta]%}%n@%M%{$reset_color%}%{$fg_bold[grey]%}]%{$reset_color%} '
 if [[ "$ZSH_PROMPT_HIDE_HOST" = true ]]; then
-	local PROMPT_HOST=''
+	PROMPT_HOST=''
+fi
+local PROMPT_NIX_SHELL=''
+if [[ -v IN_NIX_SHELL ]]; then
+	PROMPT_NIX_SHELL="%{$fg_bold[grey]%}[%{$reset_color%}%{$fg[green]%}$IN_NIX_SHELL%{$reset_color%}%{$fg_bold[grey]%}]%{$reset_color%} "
 fi
 local PROMPT_PWD='%{$fg[blue]%}%c%{$reset_color%} '
 local PROMPT_GIT='${vcs_info_msg_0_}'
 local PROMPT_DELIM='%{$fg_bold[grey]%}>%{$reset_color%} '
-PROMPT="${PROMPT_HOST}${PROMPT_PWD}${PROMPT_GIT}${PROMPT_DELIM}"
+PROMPT="${PROMPT_HOST}${PROMPT_PWD}${PROMPT_GIT}${PROMPT_NIX_SHELL}${PROMPT_DELIM}"
 
 # Title
 title() {
