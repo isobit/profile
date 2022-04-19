650464526ecad2d4d1ff15fbc34214f5575a4738
Josh Glendenning <git@isobit.io>
2022-04-19 14:10:12 -0500

    Update wezterm config

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -4,9 +4,39 @@ return {
   enable_wayland = true,
   exit_behavior = "Close",
 
-  color_scheme = "Builtin Tango Dark",
   font = w.font("Fira Code"),
   font_size = 11,
+
+  color_scheme = "Builtin Tango Dark",
+  colors = {
+    tab_bar = {
+      background = "#111",
+      active_tab = {
+        bg_color = "#333",
+        fg_color = "#EEE",
+      },
+      inactive_tab = {
+        bg_color = "#222",
+        fg_color = "#EEE",
+      },
+      new_tab = {
+        bg_color = "#111",
+        fg_color = "#EEE",
+      },
+      new_tab_hover = {
+        bg_color = "#333",
+        fg_color = "#EEE",
+      },
+    },
+  },
+  window_frame = {
+    active_titlebar_bg = "#111",
+    inactive_titlebar_bg = "#111",
+    active_titlebar_border_bottom = "#111",
+    inactive_titlebar_border_bottom = "#111",
+    button_bg = "#222",
+    button_hover_bg = "#333",
+  },
 
   keys = {
     {key="c", mods="ALT", action=w.action{CopyTo="Clipboard"}},
