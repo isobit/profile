fae6ed19c0c92f079eaa0750d5e59f840b1aa9ab
Josh Glendenning <git@isobit.io>
2023-08-04 12:35:10 -0500

    wezterm: fix config indentation

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,62 +1,61 @@
 local w = require('wezterm')
 return {
-  check_for_updates = false,
-  enable_wayland = true,
-  exit_behavior = "Close",
+	check_for_updates = false,
+	enable_wayland = false,
+	exit_behavior = "Close",
 
-  audible_bell = "Disabled",
+	audible_bell = "Disabled",
 
-  font = w.font("Fira Code"),
-  font_size = 12,
-  warn_about_missing_glyphs = false,
+	font = w.font("Fira Code"),
+	font_size = 12,
+	warn_about_missing_glyphs = false,
 
-  color_scheme = "Builtin Tango Dark",
+	color_scheme = "Builtin Tango Dark",
 
-  use_fancy_tab_bar = false;
-  colors = {
-    tab_bar = {
-      background = "#112",
-      active_tab = {
-        bg_color = "#445",
-        fg_color = "#EEE",
-      },
-      inactive_tab = {
-        bg_color = "#222",
-        fg_color = "#EEE",
-      },
-      new_tab = {
-        bg_color = "#111",
-        fg_color = "#EEE",
-      },
-      new_tab_hover = {
-        bg_color = "#333",
-        fg_color = "#EEE",
-      },
-    },
-  },
-  window_frame = {
-    active_titlebar_bg = "#111",
-    inactive_titlebar_bg = "#111",
-    active_titlebar_border_bottom = "#111",
-    inactive_titlebar_border_bottom = "#111",
-    button_bg = "#222",
-    button_hover_bg = "#333",
-  },
-  window_padding = {
-	left = 0,
-	right = 0,
-	top = 0,
-	bottom = 0,
-  },
-  window_decorations = "RESIZE",
+	use_fancy_tab_bar = false;
+	colors = {
+		tab_bar = {
+			background = "#112",
+			active_tab = {
+				bg_color = "#445",
+				fg_color = "#EEE",
+			},
+			inactive_tab = {
+				bg_color = "#222",
+				fg_color = "#EEE",
+			},
+			new_tab = {
+				bg_color = "#111",
+				fg_color = "#EEE",
+			},
+			new_tab_hover = {
+				bg_color = "#333",
+				fg_color = "#EEE",
+			},
+		},
+	},
+	window_frame = {
+		active_titlebar_bg = "#111",
+		inactive_titlebar_bg = "#111",
+		active_titlebar_border_bottom = "#111",
+		inactive_titlebar_border_bottom = "#111",
+		button_bg = "#222",
+		button_hover_bg = "#333",
+	},
+	window_padding = {
+		left = 0,
+		right = 0,
+		top = 0,
+		bottom = 0,
+	},
 
-  keys = {
-    {key="c", mods="ALT", action=w.action{CopyTo="Clipboard"}},
-    {key="v", mods="ALT", action=w.action{PasteFrom="Clipboard"}},
-    {key="t", mods="ALT", action=w.action{SpawnTab="CurrentPaneDomain"}},
-    {key="[", mods="ALT", action=w.action{ActivateTabRelative=-1}},
-    {key="{", mods="ALT|SHIFT", action=w.action{MoveTabRelative=-1}},
-    {key="]", mods="ALT", action=w.action{ActivateTabRelative=1}},
-    {key="}", mods="ALT|SHIFT", action=w.action{MoveTabRelative=1}},
-  },
+	keys = {
+		{key="c", mods="ALT", action=w.action{CopyTo="Clipboard"}},
+		{key="v", mods="ALT", action=w.action{PasteFrom="Clipboard"}},
+		{key="t", mods="ALT", action=w.action{SpawnTab="CurrentPaneDomain"}},
+		{key="[", mods="ALT", action=w.action{ActivateTabRelative=-1}},
+		{key="{", mods="ALT|SHIFT", action=w.action{MoveTabRelative=-1}},
+		{key="]", mods="ALT", action=w.action{ActivateTabRelative=1}},
+		{key="}", mods="ALT|SHIFT", action=w.action{MoveTabRelative=1}},
+	},
 }
