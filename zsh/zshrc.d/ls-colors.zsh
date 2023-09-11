60f1e59e9c53e878228f0bd4bafad245e2a305ef
Josh Glendenning <joshglendenning@remind101.com>
2023-09-10 22:53:20 -0500

    Simplify zsh prompt

--- zsh/zshrc.d/ls-colors.zsh
+++ zsh/zshrc.d/ls-colors.zsh
@@ -1,10 +1,3 @@
-export LSCOLORS="exfxcxdxbxegedabagacad"
-export LS_COLORS="di=34:ln=35:so=32:pi=33:ex=31:bd=34;46:cd=34;43:su=30;41:sg=30;46:tw=30;42:ow=30;43"
-
-# if [[ "$OSTYPE" =~ "darwin" ]]; then
-# 	alias ls="ls -G"
-# else
-# 	alias ls="ls --color=auto"
-# fi
-
+# export LSCOLORS="exfxcxdxbxegedabagacad"
+# export LS_COLORS="di=34:ln=35:so=32:pi=33:ex=31:bd=34;46:cd=34;43:su=30;41:sg=30;46:tw=30;42:ow=30;43"
 zstyle ":completion:*" list-colors "$LS_COLORS"
