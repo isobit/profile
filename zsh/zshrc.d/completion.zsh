18b92554a16eae33d18152b5bcb0e9d8aee0ccc6
Josh Glendenning <git@isobit.io>
2024-01-04 17:01:57 -0600

    Clean up zsh config

--- zsh/zshrc.d/completion.zsh
+++
@@ -1,5 +0,0 @@
-fpath=($fpath ~/.zsh-functions)
-autoload -U compinit && compinit -u
-zstyle ':completion:*' menu select
-zmodload zsh/complist
-bindkey -M menuselect '^[[Z' reverse-menu-complete
--- zsh/zshrc.d/ls-colors.zsh
+++
@@ -1,3 +0,0 @@
-# export LSCOLORS="exfxcxdxbxegedabagacad"
-# export LS_COLORS="di=34:ln=35:so=32:pi=33:ex=31:bd=34;46:cd=34;43:su=30;41:sg=30;46:tw=30;42:ow=30;43"
-zstyle ":completion:*" list-colors "$LS_COLORS"
